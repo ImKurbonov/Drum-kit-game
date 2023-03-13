e71b3d5f6a9ae467a3ec7b30feda8be2a75131f1
ImKurbonov <imkurbonov996@gmail.com>
2023-03-13 16:33:45 +0500

    Ready game

---
+++ index.js
@@ -0,0 +1,58 @@
+const drums = document.querySelectorAll(".drum");
+
+drums.forEach((drum) => {
+  drum.addEventListener('click', ()=> {
+    const drumText = drum.textContent;
+    playMusic(drumText);
+    animationKey(drumText)
+  })
+});
+
+document.addEventListener('keydown', (e)=> {
+    const buttonText = e.key
+    playMusic(buttonText)
+    animationKey(buttonText)
+})
+
+// PlayMusic()
+function playMusic(key) {
+  switch (key) {
+    case "w":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      const kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+  }
+}
+
+function animationKey(key) {
+    const button = document.querySelector(`.${key}`)
+    button.classList.add("pressed")
+
+    setTimeout(()=> {
+        button.classList.remove("pressed")
+    }, 200)
+}
